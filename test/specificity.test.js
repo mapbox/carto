d1304d409d765e8e4879c257cb77a672a8a4675c
jhollinger2 <jhollinger2@gmail.com>
2014-06-23 09:08:26 -0400

    Remove unneeded change.

--- test/specificity.test.js
+++ test/specificity.test.js
@@ -9,7 +9,6 @@ var helper = require('./support/helper');
 function cleanupItem(key, value) {
     if (key === 'rules') return;
     else if (key === 'ruleIndex') return;
-    else if (key === 'subdefinitions') return;
     else if (key === 'elements') return value.map(function(item) { return item.value; });
     else if (key === 'filters') {
         var arr = [];
