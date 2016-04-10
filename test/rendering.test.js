050e11fc34fe89fe04b246e7f0a6c941fdd0fb5f
Dane Springmeyer <dane@mapbox.com>
2016-04-10 07:57:19 -0700

    amend cdf3215da74 - allow rendering without stylesheet - refs #440, #367, #430

--- test/rendering.test.js
+++ test/rendering.test.js
@@ -1,11 +1,32 @@
 var path = require('path'),
     fs = require('fs'),
+    assert = require('assert'),
     semver = require('semver');
 
 var carto = require('../lib/carto');
 var helper = require('./support/helper');
 
 describe('Rendering', function() {
+
+    it('should support rendering without Stylesheet (for non-styling/vector tile usage)', function(done) {
+
+        // note: this intentionally does not have a `"Stylesheet":[]` property
+        // so we should skip trying to validate it
+        var opts = {"name":"","description":"",
+                    "attribution":"",
+                    "center":[0,0,3],
+                    "format":"pbf",
+                    "minzoom":0,
+                    "maxzoom":6,
+                    "srs":"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over",
+                    "Layer":[],
+                    "json":"{\"vector_layers\":[]}"};
+        var renderer = new carto.Renderer(null).render(opts);
+        assert.ok(renderer);
+        done();
+    });
+
+
 helper.files('rendering', 'mml', function(file) {
     var api = null,
         filename = path.basename(file);
