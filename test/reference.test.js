81c56a733b4d600daaa6dfd00007ebbf107c2017
nebulon42 <nebulon42@mailbox.org>
2017-07-10 23:10:38 +0200

    another test for custom reference, error handling for unrecognized rule

--- test/reference.test.js
+++ test/reference.test.js
@@ -1,4 +1,5 @@
-var assert = require('assert');
+var assert = require('assert'),
+    _ = require('lodash');
 
 var carto = require('../lib/carto');
 
@@ -52,7 +53,35 @@ describe('Reference', function() {
                 reference: customRef
             }),
             output = renderer.renderMSS('#test { marker-color: #fff; }');
+        assert.ok(!_.isNil(output.msg) && output.msg.length > 0);
         assert.equal(output.msg[0].message, 'Could not use the given reference, because it does not adhere to the specification. See the documentation for details.');
+        done();
+    });
+
+    it('should error on no rules', function(done) {
+        var customRef = {
+            load: function (wanted) {
+                return {
+                    version: wanted,
+                    style: {},
+                    layer: {},
+                    symbolizers: {},
+                    colors: {},
+                    datasources: {}
+                };
+            },
+            latest: '1.5.0',
+            versions: [
+                '1.0.0',
+                '1.5.0'
+            ]
+        };
+        var renderer = new carto.Renderer({
+                reference: customRef
+            }),
+            output = renderer.renderMSS('#test { marker-width: 1; }');
+        assert.ok(!_.isNil(output.msg) && output.msg.length > 0);
+        assert.equal(output.msg[0].message, 'Unrecognized rule: marker-width.');
         done();
     });
 });
